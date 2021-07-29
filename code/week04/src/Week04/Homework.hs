6a05c8e245515931945428884c8577b72e0eb71d
dibyajyotibehera <dibyajyotibehera@gmail.com>
2021-07-29 17:23:52 +0530

    lesson 4 | homework 1

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -9,13 +9,14 @@ module Week04.Homework where
 
 import Data.Aeson            (FromJSON, ToJSON)
 import Data.Functor          (void)
-import Data.Text             (Text)
+import Data.Text             (Text,unpack)
 import GHC.Generics          (Generic)
 import Ledger
 import Ledger.Ada            as Ada
 import Ledger.Constraints    as Constraints
 import Plutus.Contract       as Contract
 import Plutus.Trace.Emulator as Emulator
+import Wallet.Emulator.Types               (Wallet (..), walletPubKey)
 
 data PayParams = PayParams
     { ppRecipient :: PubKeyHash
@@ -25,17 +26,29 @@ data PayParams = PayParams
 type PaySchema = Endpoint "pay" PayParams
 
 payContract :: Contract () PaySchema Text ()
-payContract = do
+payContract = 
+    do
     pp <- endpoint @"pay"
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
+    handleError(\err -> Contract.logInfo $ "caught" ++ unpack err) $ void $ submitTx tx
     payContract
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace _ _ = undefined -- IMPLEMENT ME!
+payTrace a1 a2 = do
+    let pkh = pubKeyHash $ walletPubKey $ Wallet 2
+    let pp1 = PayParams{ ppRecipient = pkh,
+                        ppLovelace = a1}    
+    let pp2 = PayParams{ ppRecipient = pkh,
+    ppLovelace = a2}                 
+    h <- activateContractWallet (Wallet 1) payContract
+    callEndpoint @"pay" h pp1
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"pay" h pp2
+    void $ Emulator.waitNSlots 1
+
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
